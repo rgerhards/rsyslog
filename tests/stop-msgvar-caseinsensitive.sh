5252c72e86fd2854c082037257527b8b3ae25aa7
Peter Portante <peter.portante@redhat.com>
2015-12-19 19:29:35 +0000

    Get the new test to run without hanging
    The new test now runs without hanging, but it still fails because while
    the .conf file parser is case-insensitive, it does not change the case of
    the references parsed. So the variables are used as is.  The only place
    (that I could find) which changes the case of variables is in templates,
    where variable references are forced lower case.

--- tests/stop-msgvar-caseinsensitive.sh
+++ tests/stop-msgvar-caseinsensitive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # This file is part of the rsyslog project, released  under ASL 2.0
-# make sure message variables are used case-sensitive by default
+# make sure message variables are used case-insensitive by default
 # released under ASL 2.0
 # added 2015-12-06 rgerhards
 echo ===============================================================================
-echo \[stop-msgvar-casesensitive.sh\]: testing stop statement together with case nsensitive message variables
+echo \[stop-msgvar-casesensitive.sh\]: testing stop statement together with case-insensitive message variables
 . $srcdir/diag.sh init
-. $srcdir/diag.sh startup stop-msgvar-casesensitive.conf
+. $srcdir/diag.sh startup stop-msgvar-caseinsensitive.conf
 sleep 1
 . $srcdir/diag.sh tcpflood -m2000 -i1
 . $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
