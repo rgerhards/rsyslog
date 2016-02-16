f8741061221a9c80765d7f17cfa07d46ff6258b2
Rainer Gerhards <rgerhards@adiscon.com>
2016-02-16 14:41:09 +0100

    buildbot solaris: ensure that always "gmake" is used instead of "make"

--- tests/solaris/prep-libfastjson.sh
+++ tests/solaris/prep-libfastjson.sh
@@ -2,13 +2,19 @@
 set -e
 set -o xtrace
 PWD_HOME=$PWD
+alias make=gmake
 mkdir local_env
+mkdir local_env/bin
 mkdir local_env/install
+echo "gmake" > local_env/bin/make
+chmod +x local_env/bin/make
+PATH="$PATH:$PWD/local_env/bin"
 cd local_env
 pwd
 git clone git://github.com/rsyslog/libfastjson
 cd libfastjson
 autoreconf -fvi
+cat ./configure
 ./configure --prefix=$PWD_HOME/local_env/install
 gmake
 gmake install
