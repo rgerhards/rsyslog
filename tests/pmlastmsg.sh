478ecdcee85c7828e7094da5fae2c043dd8ff683
Rainer Gerhards <rgerhards@adiscon.com>
2016-04-26 12:57:01 +0200

    testbench: re-enale pmlastmsg test
    closes https://github.com/rsyslog/rsyslog/issues/929

--- tests/pmlastmsg.sh
+++ tests/pmlastmsg.sh
@@ -2,6 +2,7 @@
 echo ==============================================================================
 echo \[pmlastmsg.sh\]: tests for pmlastmsg
 . $srcdir/diag.sh init
+. $srcdir/diag.sh generate-HOSTNAME
 . $srcdir/diag.sh nettester pmlastmsg udp
 . $srcdir/diag.sh nettester pmlastmsg tcp
 . $srcdir/diag.sh exit
