43b8b7702e44b333644a649622143376069650e4
Rainer Gerhards <rgerhards@adiscon.com>
2018-11-14 12:12:10 +0100

    testbench: modernize test and remove unneeded code

--- tests/omrelp_wrong_authmode.sh
+++ tests/omrelp_wrong_authmode.sh
@@ -4,12 +4,10 @@
 generate_conf
 add_conf '
 module(load="../plugins/omrelp/.libs/omrelp")
-module(load="../plugins/imtcp/.libs/imtcp")
-input(type="imtcp" port="13514" ruleset="ruleset")
-
-ruleset(name="ruleset") {
-	action(type="omrelp" target="127.0.0.1" port="10514" tls="on" tls.authMode="INVALID_AUTH_MODE" tls.caCert="tls-certs/ca.pem" tls.myCert="tls-certs/cert.pem" tls.myPrivKey="tls-certs/key.pem" tls.permittedPeer=["rsyslog-test-root-ca"])
-}
+action(type="omrelp" target="127.0.0.1" port="10514" tls="on"
+	tls.authMode="INVALID_AUTH_MODE" tls.caCert="tls-certs/ca.pem"
+	tls.myCert="tls-certs/cert.pem" tls.myPrivKey="tls-certs/key.pem"
+	tls.permittedPeer=["rsyslog-test-root-ca"])
 
 action(type="omfile" file="'$RSYSLOG_OUT_LOG'")
 '
@@ -17,11 +15,5 @@ startup
 shutdown_when_empty
 wait_shutdown
 
-grep "omrelp.* invalid auth.*mode .*INVALID_AUTH_MODE" $RSYSLOG_OUT_LOG > /dev/null
-if [ $? -ne 0 ]; then
-        echo "FAIL: expected error message from missing input file not found. $RSYSLOG_OUT_LOG is:"
-        cat -n $RSYSLOG_OUT_LOG
-        error_exit 1
-fi
-
+content_check --regex "omrelp.* invalid auth.*mode .*INVALID_AUTH_MODE"
 exit_test
