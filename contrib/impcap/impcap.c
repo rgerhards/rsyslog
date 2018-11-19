48767c3bf290b6d06e56b0a3779d89bd542086af
Théo Bertin <bertintheo@orange.fr>
2018-11-19 15:38:09 +0100

    first structure

--- contrib/impcap/impcap.c
+++ contrib/impcap/impcap.c
@@ -1,9 +1,9 @@
 /* impcap.c
  *
- * This is a first implementation of an input module using libpcap, a 
+ * This is a first implementation of an input module using libpcap, a
  * portable C/C++ library for network traffic capture.
  * This module aims to read packets received from a network interface
- * using libpcap to extract information, such as IP addresses, ports, 
+ * using libpcap to extract information, such as IP addresses, ports,
  * protocols, etc... and make it available to rsyslog and its modules.
  *
  * File begun on 2018-11-13
@@ -24,3 +24,228 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ #include "config.h"
+ #include <stdlib.h>
+ #include <assert.h>
+ #include <string.h>
+ #include <errno.h>
+ #include <unistd.h>
+ #include <stdarg.h>
+ #include <ctype.h>
+
+ #include "rsyslog.h"
+ #include "errmsg.h"
+ #include "unicode-helper.h"
+ #include "module-template.h"
+ #include "rainerscript.h"
+ #include "ruleset.h"
+ #include "rsconf.h"
+ #include "dirty.h"
+ #include "msg.h"
+
+
+MODULE_TYPE_INPUT
+MODULE_TYPE_NOKEEP
+MODULE_CNFNAME("impcap")
+
+/* static data */
+DEF_IMOD_STATIC_DATA
+DEFobjCurrIf(ruleset)
+
+static rsRetVal resetConfigVariables(uchar __attribute__((unused)) *pp, void __attribute__((unused)) *pVal);
+
+/* conf structures */
+
+struct instanceConf_s {
+  uchar *interface;
+  uchar *pszBindRuleset;
+  ruleset_t *pBindRuleset;
+  struct instanceConf_s *next;
+};
+
+struct modConfData_s {
+  rsconf_t *pConf;
+  instanceConf_t *root, *tail;
+  int dummy_variable;
+};
+
+static modConfData_t *loadModConf = NULL;/* modConf ptr to use for the current load process */
+//static modConfData_t *runModConf = NULL;/* modConf ptr to use for the current running process */
+
+/* input instance parameters */
+static struct cnfparamdescr inppdescr[] = {
+	{ "dummy_variable2", eCmdHdlrPositiveInt, CNFPARAM_REQUIRED }
+};
+static struct cnfparamblk inppblk =
+	{ CNFPARAMBLK_VERSION,
+	  sizeof(inppdescr)/sizeof(struct cnfparamdescr),
+	  inppdescr
+	};
+
+/* module-global parameters */
+static struct cnfparamdescr modpdescr[] = {
+	{ "dummy_variable", eCmdHdlrPositiveInt, 0 }
+};
+static struct cnfparamblk modpblk =
+	{ CNFPARAMBLK_VERSION,
+	  sizeof(modpdescr)/sizeof(struct cnfparamdescr),
+	  modpdescr
+	};
+
+#include "im-helper.h"
+
+/* create input instance, set default parameters, and
+ * add it to the list of instances.
+ */
+static rsRetVal
+createInstance(instanceConf_t **pinst)
+{
+	instanceConf_t *inst;
+	DEFiRet;
+	CHKmalloc(inst = malloc(sizeof(instanceConf_t)));
+	inst->next = NULL;
+  inst->interface = NULL;
+
+	/* node created, let's add to config */
+	if(loadModConf->tail == NULL) {
+		loadModConf->tail = loadModConf->root = inst;
+	} else {
+		loadModConf->tail->next = inst;
+		loadModConf->tail = inst;
+	}
+
+	*pinst = inst;
+finalize_it:
+	RETiRet;
+}
+
+/* input instances */
+
+BEGINnewInpInst
+  struct cnfparamvals *pvals;
+  instanceConf_t *inst;
+  int i;
+CODESTARTnewInpInst
+  pvals = nvlstGetParams(lst, &inppblk, NULL);
+
+  CHKiRet(createInstance(&inst));
+
+  for(i = 0 ; i < inppblk.nParams ; ++i) {
+    if(!pvals[i].bUsed)
+      continue;
+    if(!strcmp(inppblk.descr[i].name, "interface")) {
+      inst->interface = (uchar*) es_str2cstr(pvals[i].val.d.estr, NULL);
+    }
+  }
+
+finalize_it:
+CODE_STD_FINALIZERnewInpInst
+  cnfparamvalsDestruct(pvals, &inppblk);
+ENDnewInpInst
+
+/* global mod conf (v2 system) */
+
+BEGINsetModCnf
+  struct cnfparamvals *pvals = NULL;
+  int i;
+CODESTARTsetModCnf
+  pvals = nvlstGetParams(lst, &modpblk, NULL);
+
+  for(i = 0 ; i < modpblk.nParams ; ++i) {
+    if(!pvals[i].bUsed)
+      continue;
+    if(!strcmp(modpblk.descr[i].name, "dummy_variable")) {
+      loadModConf->dummy_variable = (int) pvals[i].val.d.n;
+    }
+    else {
+      dbgprintf("impcap: non-handled param %s in beginCnfLoad\n", modpblk.descr[i].name);
+    }
+  }
+ENDsetModCnf
+
+/* config v2 system */
+
+/* function to generate error message if framework does not find requested ruleset */
+static inline void
+std_checkRuleset_genErrMsg(__attribute__((unused)) modConfData_t *modConf, instanceConf_t *inst)
+{
+	LogError(0, NO_ERRCODE, "impcap: ruleset not found for instance %s", inst->interface);
+}
+
+BEGINbeginCnfLoad
+CODESTARTbeginCnfLoad
+  loadModConf = pModConf;
+  loadModConf->pConf = pConf;
+  loadModConf->dummy_variable = 42;
+
+  resetConfigVariables(NULL, NULL);
+ENDbeginCnfLoad
+
+BEGINendCnfLoad
+CODESTARTendCnfLoad
+ENDendCnfLoad
+
+BEGINcheckCnf
+  instanceConf_t *inst;
+CODESTARTcheckCnf
+  for(inst = pModConf->root ; inst != NULL ; inst = inst->next) {
+    std_checkRuleset(pModConf, inst);
+  }
+  if(pModConf->root == NULL) {
+    LogError(0, RS_RET_NO_LISTNERS , "impcap: module loaded, but "
+        "no interface defined - no input will be gathered");
+    iRet = RS_RET_NO_LISTNERS;
+  }
+ENDcheckCnf
+
+BEGINactivateCnf
+CODESTARTactivateCnf
+ENDactivateCnf
+
+BEGINfreeCnf
+CODESTARTfreeCnf
+ENDfreeCnf
+
+/* runtime functions */
+
+BEGINrunInput
+CODESTARTrunInput
+ENDrunInput
+
+BEGINwillRun
+CODESTARTwillRun
+ENDwillRun
+
+BEGINafterRun
+CODESTARTafterRun
+ENDafterRun
+
+BEGINmodExit
+CODESTARTmodExit
+objRelease(ruleset, CORE_COMPONENT);
+ENDmodExit
+
+
+static rsRetVal
+resetConfigVariables(uchar __attribute__((unused)) *pp, void __attribute__((unused)) *pVal)
+{
+	return RS_RET_OK;
+}
+
+/* declaration of functions */
+
+BEGINqueryEtryPt
+CODESTARTqueryEtryPt
+CODEqueryEtryPt_STD_IMOD_QUERIES
+CODEqueryEtryPt_STD_CONF2_QUERIES
+CODEqueryEtryPt_STD_CONF2_setModCnf_QUERIES
+CODEqueryEtryPt_STD_CONF2_IMOD_QUERIES
+// CODEqueryEtryPt_STD_CONF2_PREPRIVDROP_QUERIES /* might need it */
+ENDqueryEtryPt
+
+BEGINmodInit()
+CODESTARTmodInit
+  *ipIFVersProvided = CURR_MOD_IF_VERSION;
+  CHKiRet(objUse(ruleset, CORE_COMPONENT));
+ENDmodInit
