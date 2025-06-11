d3d5c1dc921e410d62d2391c9367189e6569acff
Rainer Gerhards <rgerhards@adiscon.com>
2025-06-11 09:25:39 +0200

    Run clang analysis in 24.04 container
    - use default clang without version\n- provide valgrind suppressions env\n\nAI-Agent: Codex 2025-06

--- devtools/devcontainer.sh
+++ devtools/devcontainer.sh
@@ -64,6 +64,9 @@ docker run $ti $optrm $DOCKER_RUN_EXTRA_OPTS \
 	-e USE_AUTO_DEBUG \
 	-e RSYSLOG_STATSURL \
 	-e VCS_SLUG \
+	-e SCAN_BUILD_CC \
+	-e SCAN_BUILD \
+	-e SCAN_BUILD_REPORT_DIR \
 	--cap-add SYS_ADMIN \
 	--cap-add SYS_PTRACE \
 	${RSYSLOG_CONTAINER_UID--u $(id -u):$(id -g)} \
