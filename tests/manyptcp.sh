f003b4fc18e86211bf21722d467f0f8936809d7e
Rainer Gerhards <rgerhards@adiscon.com>
2026-04-16 18:30:22 +0200

    tests: set manyptcp maxsessions explicitly

--- tests/manyptcp.sh
+++ tests/manyptcp.sh
@@ -5,11 +5,13 @@
 . ${srcdir:=.}/diag.sh init
 export NUMMESSAGES=40000
 export QUEUE_EMPTY_CHECK_FUNC=wait_file_lines
+MAXSESSIONS=1000
 generate_conf
 add_conf '
 $MaxOpenFiles 2000
 module(load="../plugins/imptcp/.libs/imptcp")
-input(type="imptcp" SocketBacklog="1000" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port")
+input(type="imptcp" SocketBacklog="1000" maxsessions="'$MAXSESSIONS'" port="0"
+      listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port")
 
 template(name="outfmt" type="string" string="%msg:F,58:2%\n")
 :msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
