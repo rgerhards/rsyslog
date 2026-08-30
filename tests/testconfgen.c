bdde9aa2bb1ffb15ca0f83ec4abdda32a4c4cea9
Rainer Gerhards <rgerhards@adiscon.com>
2026-08-30 08:22:17 +0200

    tests: unwind standalone helper failures
    Return errors through main in the configuration generator, random data generator, and journal helper. Close opened streams and release generated configuration strings on failure instead of terminating from nested code.

--- tests/testconfgen.c
+++ tests/testconfgen.c
@@ -43,10 +43,13 @@ void pows(int n, int i) {
 
 int main(int argc, char *argv[]) {
     int n;
+    int ret = 0;
     char iname[512];
     char istmt[2048];
     int nscanned;
 
+    (void)argc;
+    (void)argv;
     n = 0;
     while (!feof(stdin)) {
         nscanned = scanf("%s %[^\n]s\n", iname, istmt);
@@ -54,15 +57,33 @@ int main(int argc, char *argv[]) {
             break;
         else if (nscanned != 2) {
             fprintf(stderr, "problem scanning entry %d, scanned %d\n", n, nscanned);
-            exit(1);
+            ret = 1;
+            break;
+        }
+        if (n >= (int)(sizeof(name) / sizeof(name[0]))) {
+            fprintf(stderr, "too many entries (maximum %zu)\n", sizeof(name) / sizeof(name[0]));
+            ret = 1;
+            break;
         }
         name[n] = strdup(iname);
         stmt[n] = strdup(istmt);
+        if (name[n] == NULL || stmt[n] == NULL) {
+            fprintf(stderr, "out of memory while storing entry %d\n", n);
+            ret = 1;
+            break;
+        }
         n++;
         printf("name: %s, stmt: %s\n", iname, istmt);
     }
     /* n is on to high for an index, but just right as the actual number! */
 
-    printf("read %d entries\n", n);
-    pows(n, n);
+    if (ret == 0) {
+        printf("read %d entries\n", n);
+        pows(n, n);
+    }
+    for (size_t i = 0; i < sizeof(name) / sizeof(name[0]); ++i) {
+        free(name[i]);
+        free(stmt[i]);
+    }
+    return ret;
 }
